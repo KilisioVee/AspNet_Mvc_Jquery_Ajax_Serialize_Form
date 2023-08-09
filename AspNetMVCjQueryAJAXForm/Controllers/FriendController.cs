081e3161379d845b93ddc1942b082482b073f260
Kilisio Veronicah <kilisionicah@gmail.com>
2023-08-09 13:30:28 +0300

    First Commit

---
+++ AspNetMVCjQueryAJAXForm/Controllers/FriendController.cs
@@ -0,0 +1,82 @@
+﻿using AspNetMVCjQueryAJAXForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCjQueryAJAXForm.Controllers
+{
+    public class FriendController : Controller
+    {
+        // GET: Friend
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Friend/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Friend/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Friend/Create
+        [HttpPost]
+        public ActionResult AddFriend(FriendModel fm)
+        {
+            //Write your database insert code / activities  
+            return RedirectToAction("create");
+        }
+
+        // GET: Friend/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Friend/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Friend/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Friend/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AspNetMVCjQueryAJAXForm/Models/FriendModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVCjQueryAJAXForm.Models
+{
+    public class FriendModel
+    {
+        public string FriendName { get; set; }
+        public string Phone { get; set; }
+        public string State { get; set; }
+    }
+}
